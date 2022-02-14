fa146cc98ad5b3437fba64dffd5a4502abebc548
akazwz <akazwz@pm.me>
2022-02-14 14:40:11 +0800

    success hash big file

--- components/file/FileOptionBar.tsx
+++ components/file/FileOptionBar.tsx
@@ -1,15 +1,20 @@
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import { Button, HStack, IconButton, Text } from '@chakra-ui/react'
 import { FolderPlus, Refresh, UploadOne } from '@icon-park/react'
-import { GetUploadToken } from '../../src/api/file'
+import { GetUploadToken, IUploadFile, UploadFileToServerApi } from '../../src/api/file'
 import { useAuth } from '../../src/hooks/useAuth'
 import { UploadStatus, useQiniuUpload } from '../../src/hooks/useQiniuUpload'
+import { useHashFile } from '../../src/hooks/useHashFile'
+import { useRecoilValue } from 'recoil'
+import { prefixDirState } from '../../src/state/file'
 
 export const FileOptionBar = () => {
   const fileInputRef = useRef<HTMLInputElement | null>(null)
 
   const [chosenFile, setChosenFile] = useState<File | null>(null)
   const [uptoken, setUptoken] = useState<string | null>(null)
+
+  const prefix = useRecoilValue(prefixDirState)
 
   const handleFileInputOnChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files) return
@@ -19,11 +24,12 @@ export const FileOptionBar = () => {
   }
 
   const { token, isAuthLoading } = useAuth()
-  const { startUpload, uploadState, uploadError } = useQiniuUpload(chosenFile, uptoken)
+  const { startUpload, uploadState, uploadError, Qkey } = useQiniuUpload(chosenFile, uptoken)
+  const { isHashLoading, isHashError, hash } = useHashFile(chosenFile)
 
   useEffect(() => {
-    if (!chosenFile) return
-    if (isAuthLoading) return
+    if (!hash) return
+    /* 获取上传凭证 */
     if (!token) return
     GetUploadToken(token).then((res) => {
       if (res.status !== 200) {
@@ -36,7 +42,41 @@ export const FileOptionBar = () => {
         setUptoken(token)
       })
     })
-  }, [chosenFile, isAuthLoading, token])
+  }, [hash, token])
+
+  /*useEffect(() => {
+    if (!chosenFile) return
+    if (!hash) return
+    if (!uptoken) return
+    /!* 开始上传 *!/
+    startUpload()
+  }, [chosenFile, hash, uptoken])
+*/
+  useEffect(() => {
+    if (!Qkey) return
+    if (!hash) return
+    if (!token) return
+    if (!chosenFile) return
+
+    /* 文件信息保存到服务器 */
+    UploadFileToServerApi(token, {
+      cid: '',
+      file: true,
+      filename: chosenFile.name,
+      prefix_dir: prefix,
+      qkey: Qkey,
+      sha256: hash,
+      size: chosenFile.size,
+    }).then((res) => {
+      if (res.status !== 201) {
+        alert('error')
+      }
+      res.json().then((resData) => {
+        const {data} = resData
+        console.log(data)
+      })
+    })
+  }, [Qkey, chosenFile, hash, prefix, token])
 
   const handleUploadClick = () => {
     fileInputRef.current?.click()
@@ -72,7 +112,7 @@ export const FileOptionBar = () => {
         rounded="full"
       />
       <Text>
-        {chosenFile?.name}
+        {hash}
       </Text>
       <Button
         disabled={!chosenFile || !uptoken}
--- src/hooks/useHashFile.ts
+++ src/hooks/useHashFile.ts
@@ -1,18 +1,55 @@
 import CryptoJs from 'crypto-js'
 import encHex from 'crypto-js/enc-hex'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-export const useHashFile = (file: File) => {
+export const useHashFile = (file: File | null) => {
   const [isHashLoading, setIsHashLoading] = useState<boolean>(true)
   const [isHashError, setIsHashError] = useState<boolean>(false)
   const [hash, setHash] = useState<string | null>(null)
 
-  hashFileSha256(file).then((hashSha256) => {
-    setHash(hashSha256)
-    setIsHashLoading(false)
-  }).catch(() => {
-    setIsHashError(true)
-  })
+  useEffect(() => {
+    if (!file) return
+
+    const sha256 = CryptoJs.algo.SHA256.create()
+
+    const hashFile = (file: File) => {
+      const chunkSize = 1024
+      let promise = Promise.resolve()
+      for (let index = 0; index < file.size; index += chunkSize) {
+        promise = promise.then(() => hashBlob(file.slice(index, index + chunkSize)))
+      }
+
+      function arrayBufferToWordArray (ab: ArrayBuffer) {
+        const i8a = new Uint8Array(ab)
+        const a = []
+        for (let i = 0; i < i8a.length; i += 4) {
+          a.push(i8a[i] << 24 | i8a[i + 1] << 16 | i8a[i + 2] << 8 | i8a[i + 3])
+        }
+        return CryptoJs.lib.WordArray.create(a, i8a.length)
+      }
+
+      const hashBlob = (blob: Blob) => {
+        return new Promise<void>((resolve, reject) => {
+          const reader = new FileReader()
+          reader.onload = ({ target }) => {
+            if (!target!.result) return
+            if (typeof target!.result === 'string') return
+            const wordArray = arrayBufferToWordArray(target!.result)
+            // 增量更新计算结果
+            sha256.update(wordArray)
+            resolve()
+          }
+          reader.readAsArrayBuffer(blob)
+        })
+      }
+
+      return promise.then(() => encHex.stringify(sha256.finalize()))
+    }
+
+    hashFile(file).then((sha256Hash) => {
+      setHash(sha256Hash)
+    })
+  }, [file])
 
   return {
     isHashLoading,
@@ -21,31 +58,4 @@ export const useHashFile = (file: File) => {
   }
 }
 
-const sha256 = CryptoJs.algo.SHA256.create()
 
-const hashFileSha256 = (file: File) => {
-  const chunkSize = 20 * 1024 * 1024
-
-  let promise = Promise.resolve<unknown>(undefined)
-
-  for (let index = 0; index < file.size; index += chunkSize) {
-    promise = promise.then(() => hashBlob(file.slice(index, index + chunkSize)))
-  }
-
-  function hashBlob (blob: Blob) {
-    return new Promise<void>((resolve, reject) => {
-      const reader = new FileReader()
-      reader.onload = ({ target }) => {
-        if (target!.result) {
-          sha256.update(target!.result!.toString())
-          resolve()
-        } else {
-          reject()
-        }
-      }
-      reader.readAsArrayBuffer(blob)
-    })
-  }
-
-  return promise.then(() => encHex.stringify(sha256.finalize()))
-}
