4ccf48c4367a6ad14fbf02b9e5b68ea29944f87e
akazwz <akazwz@pm.me>
2022-02-15 23:07:51 +0800

    add UploadFileModal

--- components/file/FileOptionBar.tsx
+++ components/file/FileOptionBar.tsx
@@ -1,108 +1,21 @@
-import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import {
-  Button, FormControl, FormLabel,
   HStack,
-  IconButton, Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton, ModalFooter, ModalHeader,
-  ModalOverlay,
-  useDisclosure
+  IconButton,
 } from '@chakra-ui/react'
-import { FolderPlus, Refresh, UploadOne } from '@icon-park/react'
-import { useFileHashCode } from 'use-hashcode'
-import { GetUploadToken, UploadFileToServerApi } from '../../src/api/file'
-import { useAuth } from '../../src/hooks/useAuth'
-import { useQiniuUpload } from '../../src/hooks/useQiniuUpload'
-import { useRecoilValue } from 'recoil'
-import { prefixDirState } from '../../src/state/file'
+import { Refresh } from '@icon-park/react'
 import { useFileList } from '../../src/hooks/useFileList'
 import { CreateFolderModal } from './CreateFolderModal'
+import { UploadFileModal } from './UploadFileModal'
 
 export const FileOptionBar = () => {
-  const fileInputRef = useRef<HTMLInputElement | null>(null)
-  const [chosenFile, setChosenFile] = useState<File | null>(null)
-  const [uptoken, setUptoken] = useState<string | null>(null)
-
-  const prefix = useRecoilValue(prefixDirState)
   const { refresh } = useFileList()
-  const { token } = useAuth()
-  const { startUpload, Qkey } = useQiniuUpload(chosenFile, uptoken)
-  const { sha256 } = useFileHashCode(chosenFile)
-
-  const { isOpen, onOpen, onClose } = useDisclosure()
-
-  /* 获取上传凭证 */
-  useEffect(() => {
-    if (!sha256) return
-    if (!token) return
-    GetUploadToken(token).then((res) => {
-      if (res.status !== 200) {
-        alert('get upload token error')
-        return
-      }
-      res.json().then((dataRes) => {
-        const { data } = dataRes
-        const { token } = data
-        setUptoken(token)
-      })
-    })
-  }, [sha256, token])
-
-  /* 文件信息保存到服务器 */
-  useEffect(() => {
-    if (!Qkey) return
-    if (!sha256) return
-    if (!token) return
-    if (!chosenFile) return
-    UploadFileToServerApi(token, {
-      cid: '',
-      file: true,
-      filename: chosenFile.name,
-      prefix_dir: prefix,
-      qkey: Qkey,
-      sha256: sha256,
-      size: chosenFile.size,
-    }).then((res) => {
-      if (res.status !== 201) {
-        alert('error')
-        return
-      }
-      refresh()
-    })
-  }, [Qkey, chosenFile, sha256, prefix, token])
-
-  /* 选择文件 */
-  const handleUploadClick = () => {
-    fileInputRef.current?.click()
-  }
-
-  const handleFileInputOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.files) return
-    /* 单个文件 */
-    const file = event.target.files[0]
-    setChosenFile(file)
-  }
-
   return (
     <HStack
       p={3}
       spacing={7}
       display={{ base: 'none', md: 'flex' }}
     >
-      <Button
-        leftIcon={<UploadOne/>}
-        colorScheme={'blue'}
-        onClick={handleUploadClick}
-      >
-        Upload
-      </Button>
-      <input
-        ref={fileInputRef}
-        type="file"
-        onChange={handleFileInputOnChange}
-        hidden
-      />
+      <UploadFileModal/>
       <CreateFolderModal/>
       <IconButton
         aria-label={''}
@@ -110,13 +23,6 @@ export const FileOptionBar = () => {
         rounded="full"
         onClick={refresh}
       />
-      <Button
-        disabled={!chosenFile || !uptoken || !sha256}
-        onClick={startUpload}
-      >
-        Start
-      </Button>
-
     </HStack>
   )
 }
---
+++ components/file/UploadFileModal.tsx
@@ -0,0 +1,126 @@
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { useRecoilValue } from 'recoil'
+import {
+  Center,
+  Button, FormControl, FormLabel,
+  Modal, ModalBody,
+  ModalCloseButton,
+  ModalContent, ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from '@chakra-ui/react'
+import { useFileHashCode } from 'use-hashcode'
+import { UploadOne } from '@icon-park/react'
+import { useAuth } from '../../src/hooks/useAuth'
+import { prefixDirState } from '../../src/state/file'
+import { useFileList } from '../../src/hooks/useFileList'
+import { GetUploadToken, UploadFileToServerApi } from '../../src/api/file'
+import { useQiniuUpload } from '../../src/hooks/useQiniuUpload'
+
+export const UploadFileModal = () => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const [chosenFile, setChosenFile] = useState<File | null>(null)
+  const [uptoken, setUptoken] = useState<string | null>(null)
+  const { startUpload, Qkey } = useQiniuUpload(chosenFile, uptoken)
+  const { sha256 } = useFileHashCode(chosenFile)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { token } = useAuth()
+  const prefixDir = useRecoilValue(prefixDirState)
+  const { refresh } = useFileList()
+
+  const handleFileInputOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return
+    /* 单个文件 */
+    const file = event.target.files[0]
+    setChosenFile(file)
+  }
+
+  /* 获取上传凭证 */
+  useEffect(() => {
+    if (!sha256) return
+    if (!token) return
+    GetUploadToken(token).then((res) => {
+      if (res.status !== 200) {
+        alert('get upload token error')
+        return
+      }
+      res.json().then((dataRes) => {
+        const { data } = dataRes
+        const { token } = data
+        setUptoken(token)
+      })
+    })
+  }, [sha256, token])
+
+  /* 文件信息保存到服务器 */
+  useEffect(() => {
+    if (!Qkey) return
+    if (!sha256) return
+    if (!token) return
+    if (!chosenFile) return
+    UploadFileToServerApi(token, {
+      cid: '',
+      file: true,
+      filename: chosenFile.name,
+      prefix_dir: prefixDir,
+      qkey: Qkey,
+      sha256: sha256,
+      size: chosenFile.size,
+    }).then((res) => {
+      if (res.status !== 201) {
+        alert('error')
+        return
+      }
+      refresh()
+    })
+  }, [Qkey, chosenFile, sha256, prefixDir, token])
+
+  return (
+    <>
+      <Button
+        leftIcon={<UploadOne/>}
+        colorScheme={'blue'}
+        onClick={onOpen}
+      >
+        Upload
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay/>
+        <ModalContent>
+          <ModalCloseButton/>
+          <ModalHeader>
+            Upload File
+          </ModalHeader>
+          <ModalBody>
+            <Center
+              p={10}
+              rounded={'lg'}
+              borderWidth={'3px'}
+              borderStyle={'dotted'}
+              onClick={()=>{fileInputRef.current?.click()}}
+            >
+              Choose File
+            </Center>
+            <input
+              ref={fileInputRef}
+              type="file"
+              onChange={handleFileInputOnChange}
+              hidden
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              disabled={!token || !chosenFile || !uptoken}
+              onClick={startUpload}
+            >
+              Start Upload
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
